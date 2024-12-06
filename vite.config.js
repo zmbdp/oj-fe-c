6866a01563e0d176f0145bbb7962f7778d8c47e7
稚名不带撇 <3566983494@qq.com>
2024-12-06 09:20:12 +0800

    spring-oj前端c端

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,24 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    proxy: {
+      "/dev-api": {
+        target: "http//127.0.0.1:19090/friend",
+        rewrite: (p) => p.replace(/^\/dev-api/, ""),
+      },
     },
   },
 })
